43df6ecf190f5b2792af71c9f354d6f66a917c3c
Pedro Honorato <111890943+honoratox@users.noreply.github.com>
2024-08-02 10:10:58 -0300

    Update automationWebSiteInscription.py

--- automationWebSiteInscription.py
+++ automationWebSiteInscription.py
@@ -8,15 +8,20 @@ import time # no meu caso o webdrive tava fechando muito rápido, então vou aum
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 servico = Service(ChromeDriverManager().install()) #instalar o ChromeDriveManager correspondente ao atual
+#fim da configuração padrão
+
+
+name_key = "Name LastName"
+email_key = "xxxx@gmail.com"
 
 navegador = webdriver.Chrome(service=servico) #definindo qual vai ser o navegador que vou usar
 
 navegador.get("https://pages.hashtagtreinamentos.com/arquivo-python-1jGh7kZSxQLoznA_GgwnWa8f7LEl3kKCZ?origemurl=hashtag_yt_org_planilhapyt_8AMNaVt0z_M") #com o naveagdor criado, eu passo a url de onde quero acessar
 
 #identificar um elemento da tela e conseguir preencher esse elemento
-navegador.find_element('xpath', '//*[@id="section-17877670"]/section/div[2]/div/form/div[1]/div/div/div/input').send_keys("pedrom.honorato@gmail,com") #'find_element' - te permite buscar um elemento especifico no site que vc anexou
-                                                                                                                            # 'xpath' - é um item padrão de todo site, mas ele tem uma id que permite a voce buscar por um item especificoo desde que siga um passo a passo pra achar esse id. 1- va no site, 2- f12(inspecionar), 3- selecionar qual parte vc quer automatizar, 4- clicar combotão direito, 5- copy -> copy xpath
-                                                                                                                            # 'send_keys' - envia(send) o que vc quer completar(keys) no elemento, nesse caso um email
+navegador.find_element('xpath', '//*[@id="section-17877670"]/section/div[2]/div/form/div[1]/div/div/div/input').send_keys(email_key) #'find_element' - te permite buscar um elemento especifico no site que vc anexou
+                                                                                                                                                       # 'xpath' - é um item padrão de todo site, mas ele tem uma id que permite a voce buscar por um item especificoo desde que siga um passo a passo pra achar esse id. 1- va no site, 2- f12(inspecionar), 3- selecionar qual parte vc quer automatizar, 4- clicar combotão direito, 5- copy -> copy xpath
+                                                                                                                                                       # 'send_keys' - envia(send) o que vc quer completar(keys) no elemento, nesse caso um email
 
 navegador.find_element('xpath', '//*[@id="section-17877670"]/section/div[2]/div/form/button/span/b').click() # 'click' - fiz o que queria e quero clicar
 
